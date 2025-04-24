ac2531cee16f3c824a72fdb576656aead90a6e7f
felipebuso <felipe.buso@hotmail.com>
2025-04-24 15:47:01 -0300

    feat: Cliente Service Implementacao

--- src/main/java/dio/projeto/design/patterns/gof/service/impl/ClienteServiceImpl.java
+++ src/main/java/dio/projeto/design/patterns/gof/service/impl/ClienteServiceImpl.java
@@ -23,44 +23,64 @@ import org.springframework.stereotype.Service;
 public class ClienteServiceImpl implements ClienteService {
 
     // Singleton: Injetar os componentes do Spring com @Autowired.
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private ViaCepService viaCepService;
+
     // Strategy: Implementar os métodos definidos na interface.
     // Facade: Abstrair integrações com subsistemas, provendo uma interface simples.
 
     @Override
     public Iterable<Cliente> buscarTodos() {
         // Buscar todos os Clientes.
-        return null;
+        return clienteRepository.findAll();
     }
 
     @Override
     public Cliente buscarPorId(Long id) {
+        Optional<Cliente> cliente = clienteRepository.findById(id);
         // Buscar Cliente por ID.
-        return null;
+        return cliente.get();
     }
 
     @Override
     public void inserir(Cliente cliente) {
-        //TODO
+        salvarClienteComCep(cliente);
     }
+
 
     @Override
     public void atualizar(Long id, Cliente cliente) {
         // Buscar Cliente por ID, caso exista:
-
+        Optional<Cliente> clienteBd = clienteRepository.findById(id);
+        if (clienteBd.isPresent()) {
+            salvarClienteComCep(cliente);
+        }
     }
 
     @Override
     public void deletar(Long id) {
-        // Deletar Cliente por ID.
-
+        clienteRepository.deleteById(id);
     }
 
     private void salvarClienteComCep(Cliente cliente) {
-        // Verificar se o Endereco do Cliente já existe (pelo CEP).
-
-
-        // Inserir Cliente, vinculando o Endereco (novo ou existente).
-        ;
+        // Verificar se o Endereço do cleinte já existe (pelo CEP)
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+            // Caso não exista, integrar com o ViaCEP e persistir o retorno
+            Endereco novoEndereco = viaCepService.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        // inserir o cliente
+        clienteRepository.save(cliente);
     }
+
 
 }
