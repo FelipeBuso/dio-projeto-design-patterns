305205e99430f6634f087c03c36f761e07539953
felipebuso <felipe.buso@hotmail.com>
2025-04-24 10:28:45 -0300

    feat: Models

--- src/main/java/dio/projeto/design/patterns/Application.java
+++ src/main/java/dio/projeto/design/patterns/Application.java
@@ -2,7 +2,18 @@ package dio.projeto.design.patterns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * Projeto Spring Boot gerado via Spring Initializr
+ * - Spring Data JPA
+ * - Spring Web
+ * - H2 Database
+ * - OpenFeign
+ *
+ * @author FelipeBuso
+ */
+@EnableFeignClients
 @SpringBootApplication
 public class Application {
 
